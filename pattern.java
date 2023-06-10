95d51153580e7da5abfc01316915784836f4da59
Chayan Jana <129534348+iamchayanjana@users.noreply.github.com>
2023-06-11 00:37:17 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pattern {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
